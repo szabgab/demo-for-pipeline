8395267634d56a0659ebf64f623972593d5869d2
Gabor Szabo <gabor@szabgab.com>
2018-05-27 17:57:05 +0300

    init

---
+++ README.md
@@ -0,0 +1,31 @@
+# Demo flask application
+
+To be used for the Jenkins demo.
+
+http://demo.code-maven.com:9091/
+
+
+## Development server
+
+```
+FLASK_APP=app FLASK_DEBUG=1 flask run --host 0.0.0.0 --port 5000
+```
+
+Visit http://127.0.0.1:5000/
+
+
+## Deployed server
+
+```
+cd work
+git clone https://github.com/szabgab/demo-for-pipeline.git
+cd demo-for-pipeline
+virtualenv venv -p python --no-site-packages
+source venv/bin/activate
+pip install -r requirements.txt
+```
+
+```
+apt-get install uwsgi  uwsgi-plugin-python
+ln -s /home/gabor/work/demo-for-pipeline/uwsgi/demo.ini /etc/uwsgi/apps-enabled/
+```
---
+++ tests/test_echo.py
@@ -0,0 +1,27 @@
+import os
+import sys
+sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
+from demo import app
+
+class TestEcho(object):
+    def setup_class(self):
+        self.app = app.test_client()
+
+    def test_main_page(self):
+        rv = self.app.get('/')
+        assert rv.status == '200 OK'
+        assert b'<h1>Hello World!</h1>' in rv.data
+        assert b'<form action="/echo" method="POST">' in rv.data
+        assert b'You said: ' not in rv.data
+
+    def test_echo(self):
+        rv = self.app.post('/echo', data = { 'text' : 'Foo Bar' })
+        assert rv.status == '200 OK'
+        assert b'<h1>Hello World!</h1>' in rv.data
+        assert b'<form action="/echo" method="POST">' in rv.data
+        assert b'You said: Foo Bar' in rv.data
+
+#    def test_demo(self):
+#        assert False
+
+# vim: expandtab
